eb14ed3e561b829f2ea9dde0c84198583d025fd7
Artem Cheban <artem.cheban@eleks.com>
2019-03-19 11:06:24 +0200

    initital commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,51 @@ function Welcome(data) {
   return <h2>Hi, {data.name}</h2>;
 }
 
+class Player extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      players : []
+    }
+  }
+
+
+
+  componentDidMount() {
+    fetch('https://api.opendota.com/api/proPlayers').then(
+        results => {
+          return results.json();
+        }
+    ).then(
+        data => {
+          let players = data.map((pic) => {
+            return (
+                <div key={pic.account_id} className='player col-md-4'>
+
+                      <span>{pic.name}</span>
+                  <br/>
+                      <span>{pic.team_name}</span>
+
+                </div>
+            )
+          });
+          this.setState({pictures: players});console.log('state = ', this.state.players);
+        }
+
+    )
+
+
+  }
+  render() {
+    return (
+        <div className='col-md-12'>
+          {this.state.pictures}
+        </div>
+
+    )
+  }
+}
+
 class Clock extends React.Component {
   constructor (props) {
     super(props);
@@ -36,23 +81,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Welcome name="willcomen"/>
-          <Welcome name="willcos"/>
         <Clock/>
-        </header>
+        <div className="container">
+          <div className="row">
+            <Player/>
+          </div>
+        </div>
       </div>
     );
   }
