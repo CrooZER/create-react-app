9c776f1c7ad3627435166f8de3ab3f2a71870cf6
Artem Cheban <artem.cheban@eleks.com>
2019-03-28 10:20:22 +0200

    search

--- src/App.js
+++ src/App.js
@@ -1,97 +1,129 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
 function Welcome(data) {
-  return <h2>Hi, {data.name}</h2>;
+    return <h2>Hi, {data.name}</h2>;
+}
+
+class Search extends React.Component {
+    state = {
+        query: '',
+    }
+
+    handleInputChange = (e) => {
+        console.log(e.target.value.toLowerCase());
+        this.setState({
+            value: this.state.value
+        })
+
+    }
+
+    render() {
+        return (
+            <form>
+                <input type="text"
+                value={this.value}
+                       onChange={this.handleInputChange}
+                />
+            </form>
+        )
+    }
 }
 
 class Player extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      players : []
+    constructor(props) {
+        super(props);
+        this.state = {
+            players: [],
+            filtered: []
+        }
     }
-  }
 
 
+    componentDidMount() {
 
-  componentDidMount() {
-    fetch('https://api.opendota.com/api/proPlayers').then(
-        results => {
-          return results.json();
-        }
-    ).then(
-        data => {
-          let players = data.map((pic) => {
-            return (
-                <div key={pic.account_id} className='player col-md-4'>
-
-                      <span>{pic.name}</span>
-                  <br/>
-                      <span>{pic.team_name}</span>
-
-                </div>
-            )
-          });
-          this.setState({pictures: players});console.log('state = ', this.state.players);
+        const result = localStorage.getItem('players');
+        if (result) {
+            console.log('From Cache');
+            this.setState({players: JSON.parse(result)});
+            return ;
         }
 
-    )
+        fetch('https://api.opendota.com/api/proPlayers').then(
+            results => {
+                return results.json();
+            }
+        ).then(
+            data => {
+                let players = data;
+                console.log('players', players);
+                this.setState({players: players});
+                console.log('Save to Cache');
 
+                localStorage.setItem('players', JSON.stringify(players));
+            }
+        )
+    }
 
-  }
-  render() {
-    return (
-        <div className='col-md-12'>
-          {this.state.pictures}
-        </div>
+    render() {
+        return (
+            <div className='row'>
+                {
+                    this.state.players.map((player, i)=>
+                        <div className='playerCard col-md-4'>
+                                   <span>
+                                       {player.team_name}
+                                       <br/>
+                                       {player.name}
+                                   </span>
+                        </div>
+                    )}
 
-    )
-  }
+            </div>
+        )
+    }
 }
 
 class Clock extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {date: new Date()};
-    this.title = 'Time ';
-    this.matches = [];
-  }
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+        this.title = 'Time ';
+        this.matches = [];
+    }
 
-  componentDidMount() {
-    this.timerID = setInterval(
-        () => this.tick(), 1000
-     )
-  }
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        )
+    }
 
-  tick() {
-    this.setState({
-      date : new Date()
-    });
-  }
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
 
-  render() {
-    return ( <div> {this.title} - {this.state.date.toLocaleTimeString()}</div>);
-  }
+    render() {
+        return (<div> {this.title} - {this.state.date.toLocaleTimeString()}</div>);
+    }
 }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Clock/>
-        <div className="container">
-          <div className="row">
-            <Player/>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Search/>
+                <Clock/>
+                <div className="container">
+                    <Player/>
+                </div>
+            </div>
+        );
+    }
 }
-
 
 
 export default App;
